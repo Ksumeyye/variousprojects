a075b7d2170a6140e1653f6dcf0882ebab0f733f
Ksumeyye <sumeeyyekaya@gmail.com>
2025-01-05 01:11:13 +0300

    *

---
+++ AlisVeris/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace AlisVeris
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("server=LAPTOP-O1DRK0LF;database=AlisVeris;integrated security=true;trustServerCertificate=true");
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Execute AlisVeris",baglan);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+    }
+}
