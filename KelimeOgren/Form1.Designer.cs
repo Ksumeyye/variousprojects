8f93c8fe8ecec833cac211278d36cc413c5806a8
Ksumeyye <sumeeyyekaya@gmail.com>
2025-01-06 21:22:25 +0300

    WinFormsApp ve Access veritabanı ile İngilizce-Türkce Sözlük

---
+++ KelimeOgren/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace KelimeOgren
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtIngilizce = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtTurkce = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.LblSüre = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.LblKelime = new System.Windows.Forms.Label();
+            this.LblCevap = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İngilizce:";
+            // 
+            // TxtIngilizce
+            // 
+            this.TxtIngilizce.Location = new System.Drawing.Point(107, 34);
+            this.TxtIngilizce.Name = "TxtIngilizce";
+            this.TxtIngilizce.Size = new System.Drawing.Size(141, 26);
+            this.TxtIngilizce.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Türkçe:";
+            // 
+            // TxtTurkce
+            // 
+            this.TxtTurkce.Location = new System.Drawing.Point(107, 83);
+            this.TxtTurkce.Name = "TxtTurkce";
+            this.TxtTurkce.Size = new System.Drawing.Size(141, 26);
+            this.TxtTurkce.TabIndex = 1;
+            this.TxtTurkce.TextChanged += new System.EventHandler(this.TxtTurkce_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(379, 42);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(46, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Süre:";
+            // 
+            // LblSüre
+            // 
+            this.LblSüre.AutoSize = true;
+            this.LblSüre.Location = new System.Drawing.Point(431, 42);
+            this.LblSüre.Name = "LblSüre";
+            this.LblSüre.Size = new System.Drawing.Size(27, 18);
+            this.LblSüre.TabIndex = 5;
+            this.LblSüre.Text = "90";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(318, 87);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(107, 18);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Kelime Sayısı:";
+            // 
+            // LblKelime
+            // 
+            this.LblKelime.AutoSize = true;
+            this.LblKelime.Location = new System.Drawing.Point(431, 87);
+            this.LblKelime.Name = "LblKelime";
+            this.LblKelime.Size = new System.Drawing.Size(18, 18);
+            this.LblKelime.TabIndex = 7;
+            this.LblKelime.Text = "0";
+            // 
+            // LblCevap
+            // 
+            this.LblCevap.AutoSize = true;
+            this.LblCevap.Location = new System.Drawing.Point(375, 119);
+            this.LblCevap.Name = "LblCevap";
+            this.LblCevap.Size = new System.Drawing.Size(50, 18);
+            this.LblCevap.TabIndex = 8;
+            this.LblCevap.Text = "label4";
+            this.LblCevap.Visible = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Lime;
+            this.ClientSize = new System.Drawing.Size(566, 161);
+            this.Controls.Add(this.LblCevap);
+            this.Controls.Add(this.LblKelime);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.LblSüre);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtTurkce);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtIngilizce);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "İngilizce -Türkçe Sözlük";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtIngilizce;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtTurkce;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label LblSüre;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label LblKelime;
+        private System.Windows.Forms.Label LblCevap;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ KelimeOgren/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb; //Access için OleDb kullanılır.
+
+namespace KelimeOgren
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        OleDbConnection baglanti = new OleDbConnection(@"Provider = Microsoft.ACE.OLEDB.12.0; Data Source = C:\Users\SÜMEYYE\OneDrive\Masaüstü\dbSozluk\dbSozluk.accdb"); //@ işareti bunun bir adres olduğunu programa bildirir.
+        Random rast = new Random(); //Random sınıfından rast nesnesi oluşturdum.
+        int sure = 90;
+        int kelime = 0;
+        void getir() // getir diye method tanımladım.
+        {
+            int sayi;
+            sayi = rast.Next(1, 2490);
+            baglanti.Open();
+            OleDbCommand komut = new OleDbCommand("Select*FROM sozluk where id=@p1", baglanti);
+            komut.Parameters.AddWithValue("@p1", sayi);
+            OleDbDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                TxtIngilizce.Text = dr[1].ToString();
+                LblCevap.Text = dr[2].ToString();
+                LblCevap.Text = LblCevap.Text.ToLower();
+            }
+            baglanti.Close();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            getir();
+            TxtTurkce.Focus();
+            timer1.Start();
+        }
+
+        private void TxtTurkce_TextChanged(object sender, EventArgs e)
+        {
+            if(TxtTurkce.Text==LblCevap.Text)
+            {
+                kelime++;
+                LblKelime.Text = kelime.ToString();
+                getir();
+                TxtTurkce.Clear();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sure--;
+            LblSüre.Text=sure.ToString();
+            if( sure==0 )
+            {
+                TxtTurkce.Enabled= false;
+                TxtIngilizce.Enabled= false;
+                timer1.Stop();
+            }
+        }
+    }
+}
