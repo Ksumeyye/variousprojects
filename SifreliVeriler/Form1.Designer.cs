345a17794c69381f4e4a1cfaeb736f50834742bf
Ksumeyye <sumeeyyekaya@gmail.com>
2025-01-08 18:43:16 +0300

    WinFormsApp, SqlClient, Convert.ToBase64String ve Convert.FromBase64String şifeleme türlerini kullandığım veri şifreleme projem

---
+++ SifreliVeriler/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace SifreliVeriler
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.TxtAd = new System.Windows.Forms.TextBox();
+            this.TxtSoyad = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtSifre = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TxtHesapNo = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(-5, 175);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(806, 203);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ad:";
+            // 
+            // TxtAd
+            // 
+            this.TxtAd.Location = new System.Drawing.Point(104, 37);
+            this.TxtAd.Name = "TxtAd";
+            this.TxtAd.Size = new System.Drawing.Size(100, 20);
+            this.TxtAd.TabIndex = 2;
+            // 
+            // TxtSoyad
+            // 
+            this.TxtSoyad.Location = new System.Drawing.Point(104, 74);
+            this.TxtSoyad.Name = "TxtSoyad";
+            this.TxtSoyad.Size = new System.Drawing.Size(100, 20);
+            this.TxtSoyad.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Soyad:";
+            // 
+            // TxtMail
+            // 
+            this.TxtMail.Location = new System.Drawing.Point(104, 114);
+            this.TxtMail.Name = "TxtMail";
+            this.TxtMail.Size = new System.Drawing.Size(100, 20);
+            this.TxtMail.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(29, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Mail:";
+            // 
+            // TxtSifre
+            // 
+            this.TxtSifre.Location = new System.Drawing.Point(312, 37);
+            this.TxtSifre.Name = "TxtSifre";
+            this.TxtSifre.Size = new System.Drawing.Size(100, 20);
+            this.TxtSifre.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(275, 44);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(31, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Şifre:";
+            // 
+            // TxtHesapNo
+            // 
+            this.TxtHesapNo.Location = new System.Drawing.Point(312, 78);
+            this.TxtHesapNo.Name = "TxtHesapNo";
+            this.TxtHesapNo.Size = new System.Drawing.Size(100, 20);
+            this.TxtHesapNo.TabIndex = 10;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(248, 81);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 13);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Hesap No:";
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(312, 108);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(100, 25);
+            this.BtnKaydet.TabIndex = 11;
+            this.BtnKaydet.Text = "Kaydet";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(469, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "label6";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 25);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Şifreyi Çöz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 377);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.BtnKaydet);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TxtHesapNo);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TxtSifre);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TxtMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TxtSoyad);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TxtAd);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Veri Şifreleme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TxtAd;
+        private System.Windows.Forms.TextBox TxtSoyad;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TxtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtSifre;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtHesapNo;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SifreliVeriler/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace SifreliVeriler
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SqlConnection baglan = new SqlConnection("server=LAPTOP-O1DRK0LF;database=PersonelVeriTabani;integrated security=true;trustServerCertificate=true");
+
+        void Listele()
+        {
+            SqlDataAdapter da = new SqlDataAdapter("Select*From TblVeriler",baglan);
+            DataTable dt=new DataTable();
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+        }
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            string Ad = TxtAd.Text;
+            byte[] Addizi=ASCIIEncoding.ASCII.GetBytes(Ad); //Metinin byte olarak değerini parçalayarak al.
+            string Adsifre=Convert.ToBase64String(Addizi); //ToBase64 kullanılan şifreleme yöntemlerinden birisidir. Burada veri diziyi ToBase64 formatına çevirdim.
+            //label6.Text = adsifre; //Şifrelenen veriyi label6 içine yazdırır.
+
+            string Soyad = TxtSoyad.Text;
+            byte[] Soyaddizi = ASCIIEncoding.ASCII.GetBytes(Soyad);
+            string Soyadsifre=Convert.ToBase64String(Soyaddizi);
+
+            string Mail = TxtMail.Text;
+            byte[] Maildizi= ASCIIEncoding.ASCII.GetBytes(Mail);
+            string Mailsifre = Convert.ToBase64String(Maildizi);
+
+            string Sifre = TxtSifre.Text;
+            byte[] Sifredizi = ASCIIEncoding.ASCII.GetBytes(Sifre);
+            string Sifresifre = Convert.ToBase64String(Sifredizi);
+
+            string Hesapno = TxtHesapNo.Text;
+            byte[] Hesapnodizi = ASCIIEncoding.ASCII.GetBytes(Hesapno);
+            string Hesapnosifre = Convert.ToBase64String(Hesapnodizi);
+
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("Insert Into TblVeriler (Ad,Soyad,Mail,Sifre,HesapNo) values(@P1,@P2,@P3,@P4,@P5)",baglan);
+            komut.Parameters.AddWithValue("@P1", TxtAd.Text);
+            komut.Parameters.AddWithValue("@P2",TxtSoyad.Text);
+            komut.Parameters.AddWithValue("@P3",TxtMail.Text);
+            komut.Parameters.AddWithValue("@P4", TxtSifre.Text);
+            komut.Parameters.AddWithValue("@P5",TxtHesapNo.Text);
+            komut.ExecuteNonQuery();
+            baglan.Close();
+            MessageBox.Show("Verileriniz Başarılı Şekilde Şifrelendi.","Bilgi",MessageBoxButtons.OK,MessageBoxIcon.Information);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listele();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string Adcozum = TxtAd.Text;
+            byte[] Adcozumdizi=Convert.FromBase64String(Adcozum);
+            string Adverisi = ASCIIEncoding.ASCII.GetString(Adcozumdizi);
+            label6.Text = Adverisi;
+
+            string Soyadcozum = TxtSoyad.Text;
+            byte[] Soyadcozumdizi = Convert.FromBase64String(Soyadcozum);
+            string Soyadverisi = ASCIIEncoding.ASCII.GetString(Soyadcozumdizi);
+            label6.Text = Soyadverisi;
+
+            string Mailcozum = TxtMail.Text;
+            byte[] Mailcozumdizi = Convert.FromBase64String(Mailcozum);
+            string Mailverisi = ASCIIEncoding.ASCII.GetString(Mailcozumdizi);
+            label6.Text = Mailverisi;
+
+            string Sifrecozum = TxtSifre.Text;
+            byte[] Sifrecozumdizi = Convert.FromBase64String(Sifrecozum);
+            string Sifreverisi = ASCIIEncoding.ASCII.GetString(Sifrecozumdizi);
+            label6.Text = Sifreverisi;
+
+            string Hesapnocozum = TxtHesapNo.Text;
+            byte[] Hesapnocozumdizi = Convert.FromBase64String(Hesapnocozum);
+            string Hesapnoverisi = ASCIIEncoding.ASCII.GetString(Hesapnocozumdizi);
+            label6.Text = Hesapnoverisi;
+        }
+    }
+}
