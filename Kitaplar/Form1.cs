67418aa19e5e5872f9347d9b0d0856e0ca3b302c
Ksumeyye <sumeeyyekaya@gmail.com>
2025-02-03 14:34:08 +0300

    OOP + MSSql ile Class yapısıyla Kitaplık adlı projemdir.

---
+++ Kitaplar/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kitaplar
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+      KitapVT vtSinif = new KitapVT();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = vtSinif.Liste();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Kitap ktpsinif=new Kitap();
+            ktpsinif.KitapAd = textBox1.Text;
+            ktpsinif.Yazar = textBox2.Text;
+            vtSinif.KitapEkle(ktpsinif);
+            MessageBox.Show("Kitap Eklendi");
+        }
+    }
+}
---
+++ Kitaplar/KitapVT.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace Kitaplar
+{ 
+    class KitapVT
+    {
+        SqlConnection baglan = new SqlConnection("server=LAPTOP-O1DRK0LF;database=Kitaplik;integrated security=true;trustServerCertificate=true");
+
+        public List<Kitap> Liste()
+        {
+            List<Kitap> ktp = new List<Kitap>();
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("Select * from TblKitaplar", baglan);
+            SqlDataReader dr = komut.ExecuteReader();
+            while (dr.Read())
+            {
+                Kitap k = new Kitap();
+                k.ID = Convert.ToInt16(dr[0]);
+                k.KitapAd = dr[1].ToString();
+                k.Yazar = dr[2].ToString();
+                ktp.Add(k);
+            }
+            baglan.Close();
+            return ktp;
+        }
+        public void KitapEkle(Kitap kt)
+        {
+            baglan.Open();
+            SqlCommand komut = new SqlCommand("Insert into TblKitaplar (KitapAd,Yazar) values(@p1,@p2)", baglan);
+            komut.Parameters.AddWithValue("@p1", kt.KitapAd);
+            komut.Parameters.AddWithValue("@p2", kt.Yazar);
+            komut.ExecuteNonQuery();
+            baglan.Close();
+        }
+    }
+}
